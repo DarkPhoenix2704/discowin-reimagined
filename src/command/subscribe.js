0f4e3df94db7e83b7fd3b2c4ab2893a2293792f8
Anbarasu <anbarasun123@gmail.com>
2021-06-01 18:48:04 +0530

    User Data is Stored in MongoDb

--- src/command/subscribe.js
+++ src/command/subscribe.js
@@ -1,13 +1,35 @@
 const { addUser } = require('../database')
 const userModel = require('../model/user')
+const Discord = require('discord.js')
 
-module.exports = async (client,message) => {
-    const {content, author} = message
-    const args = content.slice(process.env.PREFIX.length).trim().split(/ +/);
-    let age = args[1] ,district = args[2]
-    if(age < 0){
-        console.log('Age Can\'t be negative')
-        return
+module.exports = {
+    execute(client,message){
+        const {content, author} = message
+        const args = content.slice(process.env.PREFIX.length).trim().split(/ +/);
+        let age = args[1] ,district = args[2]
+        if(age < 0){
+            console.log('Age Can\'t be negative')
+            return
+        }
+        try{
+            await addUser(author.id,age,district)
+            const embedMessage = new Discord.MessageEmbed()
+            .setTitle('Subscribed Succesfully')
+            .setAuthor('Discowin','./bot.png','https://darkphoenix2704.github.io/discowin')
+            .addField('You are succesfully Subscribed to Cowin Notifications')
+            .setFooter('Discowin')
+            .setTimestamp()
+            message.reply(embedMessage)
+        }catch(e){
+            console.log(`Exception   ${e}`)
+            const embedMessage = new Discord.MessageEmbed()
+            .setTitle('Error Occured')
+            .setAuthor('Discowin','./bot.png','https://darkphoenix2704.github.io/discowin')
+            .addField('Some Error Occured. Please Try Again Later')
+            .setFooter('Discowin')
+            .setTimestamp()
+            message.reply(embedMessage)
+        }
+        
     }
-    await addUser(author.id,age,district)
 }
