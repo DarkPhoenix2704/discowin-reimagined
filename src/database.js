b2306c691860347d7c9758bc9d671460dcf46d15
Anbarasu <anbarasun123@gmail.com>
2021-06-01 20:52:12 +0530

    Implemented Some CRUD Functions

--- src/database.js
+++ src/database.js
@@ -4,18 +4,41 @@ const districtModel = require('./model/district')
 
 
 
-module.exports.addUser = async (_id,age,district) => {
-    let user = new userModel({_id:_id,age:age,district:district})
+module.exports.addUser = async (_id, age, district) => {
+    let user = new userModel({
+        _id: _id,
+        age: age,
+        district: district
+    })
     await user.save()
 }
 
-module.exports.removeUser = async(_id) => {
+module.exports.removeUser = async (_id) => {
     userModel.remove({
-        _id:_id
-    },(err) => console.log(err))
+        _id: _id
+    }, (err) => console.log(err))
 }
 
-module.exports.addDistrict = async (_id,district) => {
-    let _district = new districtModel({_id,district})
+module.exports.addDistrict = async (_id, district) => {
+    let _district = new districtModel({
+        _id,
+        district
+    })
     await _district.save
+}
+
+module.exports.getDistricts = async () => {
+    districtModel.find({}, async (err, data) => {
+        if (err) return null
+        return data
+    })
+}
+
+module.exports.getDistrict = async (district) => {
+    districtModel.find({
+        district: district
+    }, (err, data) => {
+        if(err) return null
+        return data
+    })
 }
