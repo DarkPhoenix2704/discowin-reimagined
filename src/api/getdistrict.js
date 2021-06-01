7c8f0fce7a608e2bbc4bad33b70ec174bbfaf000
Anbarasu <anbarasun123@gmail.com>
2021-06-01 21:14:50 +0530

    Added API calls to fetch Vaccine Availability & District List

---
+++ src/api/getdistrict.js
@@ -0,0 +1,16 @@
+const axios = require('axios')
+
+const baseUrl = 'https://cdn-api.co-vin.in/api/v2/admin/location/districts/'
+
+
+module.exports.getvaccine = async (state_id) => {
+    let requestUrl = baseUrl + state_id
+    console.log(requestUrl)
+    axios.get(requestUrl,{
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:89.0) Gecko/20100101 Firefox/89.0',
+        }
+    }).then(async value => {
+        return value.data.districts
+    })
+}
---
+++ src/api/getvaccine.js
@@ -0,0 +1,34 @@
+const axios = require('axios')
+
+const baseurl = 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id='
+
+
+module.exports.getVaccine = async (district) => {
+    let requestUrl = baseurl + district + '&date=' + getDate()
+    console.log(requestUrl)
+    axios.get(requestUrl, {
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:89.0) Gecko/20100101 Firefox/89.0',
+        }
+    }).then(async value => {
+        return value.data.sessions
+    }, async exception => {
+        console.log(exception)
+    })
+}
+
+function getDate() {
+    let today = new Date()
+    let dd = today.getDate()
+    let mm = today.getMonth() + 1
+
+    const yyyy = today.getFullYear()
+    if (dd < 10) {
+        dd = '0' + dd
+    }
+    if (mm < 10) {
+        mm = '0' + mm
+    }
+    today = dd + '-' + mm + '-' + yyyy
+    return today
+}
