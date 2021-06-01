f26dac988063ce3081629ace75b7fa81c84c17bd
Anbarasu <anbarasun123@gmail.com>
2021-06-01 18:33:35 +0530

    Implemented District Model to Store District List

--- src/database.js
+++ src/database.js
@@ -1,7 +1,11 @@
 const Discord = require('discord.js')
 const userModel = require('./model/user')
-
+const districtModel = require('./model/district')
 module.exports.addUser = async (_id,age,district) => {
     let user = new userModel({_id:_id,age:age,district:district})
     await user.save()
+}
+module.exports.addDistrict = async (_id,district) => {
+    let _district = new districtModel({_id,district})
+    await _district.save
 }
---
+++ src/model/district.js
@@ -0,0 +1,6 @@
+let mongoose = require('mongoose')
+let district = new mongoose.Schema({
+    _id:{type:Number,required:true},
+    district:{type:String,required:true}
+})
+module.exports = mongoose.Model('district',district)
