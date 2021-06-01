dc948439df10147e2a381b57c74785812629c17f
Anbarasu <anbarasun123@gmail.com>
2021-06-01 19:43:22 +0530

    Added Commands for UnSubscribing to Notifications

--- src/database.js
+++ src/database.js
@@ -1,10 +1,20 @@
 const Discord = require('discord.js')
 const userModel = require('./model/user')
 const districtModel = require('./model/district')
+
+
+
 module.exports.addUser = async (_id,age,district) => {
     let user = new userModel({_id:_id,age:age,district:district})
     await user.save()
 }
+
+module.exports.removeUser = async(_id) => {
+    userModel.remove({
+        _id:_id
+    },(err) => console.log(err))
+}
+
 module.exports.addDistrict = async (_id,district) => {
     let _district = new districtModel({_id,district})
     await _district.save
