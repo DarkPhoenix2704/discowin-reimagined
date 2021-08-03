55e99daa2feacc1c643de0818e731e0b3951c264
Anbarasu <anbarasun123@gmail.com>
2021-08-03 15:55:31 +0530

    FIxed Some Typo

--- src/command/checknow.js
+++ src/command/checknow.js
@@ -3,12 +3,16 @@ const getSessions = require('../api/getsession')
 
 
 module.exports = {
-    execute(client ,message) {
-        let {_age,district_name} = await client.database.getUser(message.author.id)
+    run: async (client, message) => {
+
+        let {
+            _age,
+            district_name
+        } = await client.database.getUser(message.author.id)
         let district_id = await client.database.getDistrict(district_name)
         let sessions = await getSessions(district_id)
         await client.database.deleteSessionsWhere(district_id)
-        for(let i = 0;i < sessions.length; i++){
+        for (let i = 0; i < sessions.length; i++) {
             let {
                 center_id,
                 district_name,
@@ -22,5 +26,6 @@ module.exports = {
             } = sessions[i]
             await client.database.addSession(center_id, district_name, name, vaccine, min_age_limit, fee_type, fee, available_capacity, date)
         }
+
     }
 }
--- src/command/help.js
+++ src/command/help.js
@@ -1,10 +1,10 @@
 const Discord = require("discord.js")
 
 module.exports = {
-    execute(client, message) {
+    run: async (client, message) => {
         const embedMessage = new Discord.MessageEmbed()
             .setTitle('Available Commands')
-            .setAuthor('Discowin', './bot.png', 'https://darkphoenix2704.github.io/discowin')
+            .setAuthor('Discowin', '', 'https://darkphoenix2704.github.io/discowin')
             .addField('!subscribe <age> <district>', 'To Subscribe for Cowin Notification')
             .addField('!unsubscribe', 'To unSubscribe from Cowin notification')
             .addField('!checknow', 'To Check for Vaccine Availability Now')
