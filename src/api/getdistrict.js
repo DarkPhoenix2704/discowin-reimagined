fd365a67a6e84ce7dad997c0147351229f241bc1
hariprasad1003 <hariprasadgurunathan10@gmail.com>
2021-08-03 19:48:27 +0530

    error fixed

--- src/api/getdistrict.js
+++ src/api/getdistrict.js
@@ -4,16 +4,23 @@ const baseUrl = 'https://cdn-api.co-vin.in/api/v2/admin/location/districts/'
 
 
 module.exports.getDistrict = async (state_id) => {
+    
     let requestUrl = baseUrl + state_id
+    
     console.log(requestUrl)
-    axios.get(requestUrl, {
+
+    const req = axios.get(requestUrl, {
+
         headers: {
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:89.0) Gecko/20100101 Firefox/89.0',
         }
-    }).then(async value => {
-        console.log(value.data.districts)
-        return value.data.districts
-    }).catch(err => {
-        console.log(err)
+    
     })
+    
+    const res = req.then((value) => value.data.districts)
+
+    const err = req.catch((err) => console.log(err))
+
+    return res
+    
 }
