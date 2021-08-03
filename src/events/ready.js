a38e2858ceb2f8ab429053b102a6b75bc840ca8f
Anbarasu <anbarasun123@gmail.com>
2021-08-03 18:16:14 +0530

    Miscs

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,4 +1,6 @@
-module.exports = (client) => {
+const updateDistrict = require('../update/updateDistrict')
+module.exports = async (client) => {
     console.log('Discowin is ready')
     client.user.setAvatar('./bot.png')
+    await updateDistrict(client);
 }
