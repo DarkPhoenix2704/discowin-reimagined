6939ccbfbd1c7f07a1a6a4ea0e6279d325eaa82f
Anbarasu <anbarasun123@gmail.com>
2021-08-25 11:02:17 +0530

    Delete Sessions when bot starts up

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,6 +1,8 @@
 const updateDistrict = require('../update/updateDistrict')
+const database = require('../database/database')
 module.exports = async (client) => {
     console.log('Discowin is ready')
     client.user.setAvatar('./bot.png')
     await updateDistrict(client);
+    await database.deleteSessions()
 }
