d2090113f442bf8816fb51ec54470bd9148dc5d8
Anbarasu <anbarasun123@gmail.com>
2021-08-25 11:06:40 +0530

    Removed unnecessary Space

--- src/api/getdistrict.js
+++ src/api/getdistrict.js
@@ -4,23 +4,15 @@ const baseUrl = 'https://cdn-api.co-vin.in/api/v2/admin/location/districts/'
 
 
 module.exports.getDistrict = async (state_id) => {
-    
     let requestUrl = baseUrl + state_id
-    
     console.log(requestUrl)
-
     const req = axios.get(requestUrl, {
-
         headers: {
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:89.0) Gecko/20100101 Firefox/89.0',
         }
-    
     })
-    
     const res = req.then((value) => value.data.districts)
-
-    const err = req.catch((err) => console.log(err))
-
+    req.catch((err) => console.log(err))
     return res
     
 }
